d3cc3745619eafe26bae0e628559f672d10d62b5
Tomasz Szkonter <Tomasz.Szkonter@gmail.com>
2021-05-28 02:58:00 +0200

    Finished boilerplate
    Tomorrow adding first components according to designed project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/MainTheme';
+import GlobalStyle from './theme/GlobalStyle';
 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <h1>ELON</h1>
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap');
+
+    *,*::before,*::after {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        margin: 0;
+        padding: 0;
+    }   
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body{
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-family: 'Open Sans', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
